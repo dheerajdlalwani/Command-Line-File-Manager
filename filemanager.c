83da4ea2ce06a6e383443bc98c0757681a9b0207
dheerajdlalwani <lalwanidheeraj1234@gmail.com>
2021-03-07 16:14:26 +0530

    :sparkles: getCurrentDateAndTime now successfully returns a pointer to a string with date and time

--- filemanager.c
+++ filemanager.c
@@ -67,29 +67,26 @@ char *getCurrentDate()
 	return currentDate;
 }
 
-// char *getCurrentDateAndTime()
-// {
-// 	char *time = getCurrentTime();
-// 	char *date = getCurrentDate();
-// 	char *dateTime = malloc(sizeof(char) * (strlen(time) + strlen(date) + 3));
-// 	dateTime[0] = 0;
-// 	strcat(dateTime, date);
-// 	strcat(dateTime, " | ");
-// 	strcat(dateTime, time);
-// 	return dateTime;
-// }
+char *getCurrentDateAndTime()
+{
+	char *time = getCurrentTime();
+	char *date = getCurrentDate();
+	char *dateTime = malloc(sizeof(char) * (strlen(time) + strlen(date) + 3));
+	dateTime[0] = 0;
+	strcat(dateTime, date);
+	strcat(dateTime, " | ");
+	strcat(dateTime, time);
+	return dateTime;
+}
 
 int main()
 {
-	// int hours, minutes, seconds, day, month, year;
-	// time_t now;
 	// FILE *filePointer;
 	// filePointer = fopen("life.txt", "a");
-	// time(&now);
 
 	printf("System's current time: %s\n", getCurrentTime());
 	printf("System's current date: %s\n", getCurrentDate());
-	// printf("System's current date and time is: %s\n", getCurrentDateAndTime());
+	printf("System's current date and time is: %s\n", getCurrentDateAndTime());
 
 	// snprintf
 	return 0;
